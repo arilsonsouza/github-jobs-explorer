c5c96b27a8f7bc506902add917c4fb8d2cf62b64
Arilson Souza <arilsons61@gmail.com>
2021-10-21 08:38:40 -0300

    update ssh key setup script

--- netlify-setup.sh
+++ netlify-setup.sh
@@ -9,13 +9,14 @@ if [ ! -z "${DEPLOY_PRIME_URL}" ]; then
     # echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
     echo -e ${SSH_KEY} | base64 --decode > ~/.ssh/id_rsa
 
-    chmod og-rwx ~/.ssh/id_rsa    
+    chmod og-rwx ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa
     # Uncomment to debug
     ls -la ~/.ssh    
     cat ~/.ssh/id_rsa
 
     # Add host keys, comment out if not needed
     ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-    ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
+    # ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
 
 fi;
